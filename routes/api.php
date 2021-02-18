c8044766a40178ef799b0d4fd8ad2a34840173d8
Luis F <luisfellipe60@gmail.com>
2021-02-18 17:34:51 -0300

    Criação das rotas API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Brand;
+use App\Models\Model;
+use App\Models\ModelPart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('brands')->group(function () {
+    Route::get('/', function () {
+        return Brand::all();
+    });
+
+    Route::get('{id}', function ($id) {
+        return Brand::findOrFail($id);
+    });
+
+    Route::get('{id}/parts', function ($id) {
+        return Brand::findOrFail($id)->parts;
+    });
+});
+
+Route::prefix('models')->group(function () {
+    Route::get('/', function () {
+        return Model::all();
+    });
+
+    Route::get('{id}', function ($id) {
+        return Model::findOrFail($id);
+    });
+
+    Route::get('{id}/parts', function($id) {
+        return Model::findOrFail($id)->parts;
+    });
+});
+
+Route::prefix('parts')->group(function () {
+    Route::get('/', function () {
+        return ModelPart::all();
+    });
+
+    Route::get('{id}', function ($id) {
+        return ModelPart::findOrFail($id);
+    });
+
+    Route::prefix('by')->group(function () {
+        Route::get('model/{id}', function ($id) {
+            return Model::findOrFail($id)->parts;
+        });
+
+        Route::get('brand/{id}', function ($id) {
+            return Brand::findOrFail($id)->parts;
+        });
+    });
 });
